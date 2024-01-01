765cda057407e208f725e33e2b68145b27f678e0
Md. Rayhan Islam Sefat <112571481+RayhanSefat@users.noreply.github.com>
2024-01-01 13:50:23 +0600

    JS chapter 1 done

---
+++ JavaScript/Codes/first.js
@@ -0,0 +1,9 @@
+const profile = {
+    name: "shradhakhapra",
+    isFollow: false,
+    post: 195,
+    followers: 569000,
+    following: 4,
+    occupation: "Enterpreneur",
+    bio: "blah.. blah..."
+};
