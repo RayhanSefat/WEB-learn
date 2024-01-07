0a10501ea8f456a26a5138a6610991375d0a3b12
Md. Rayhan Islam Sefat <112571481+RayhanSefat@users.noreply.github.com>
2024-01-07 12:55:13 +0600

    DOM2

--- JavaScript/DOM/script.js
+++ JavaScript/DOM/script.js
@@ -5,3 +5,12 @@ let boxes = document.querySelectorAll(".box");
 boxes[0].innerText = "These are"
 boxes[1].innerText = "3 boxes"
 boxes[2].innerText = "with unique texts"
+
+let newButton = document.createElement("button");
+newButton.innerText = "Click me"
+newButton.style.backgroundColor = "red";
+newButton.style.color = "white";
+document.querySelector("body").prepend(newButton);
+
+let para = document.querySelector(".paragraph");
+para.classList.add("paragraph-upd1");
